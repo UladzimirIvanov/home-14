1e0620a539acbf2946991f4618c213f8da36b876
UladzimirIvanov <ivanouum@gmail.com>
2024-05-11 16:34:14 +0300

    TaskHard 20

--- src/TaskHard/Main.java
+++ src/TaskHard/Main.java
@@ -16,10 +16,20 @@ public class Main {
             while ((i = stream.read()) != -1){
                 txtDocs.append((char) i);
             }
-            System.out.println(txtDocs);
+            //System.out.println(txtDocs);
+            //System.out.println();
+            String[] arrDocs = txtDocs.toString().split("\n");
+            for (int j = 0; j < arrDocs.length; j++) {
+                //System.out.println("Doc number " + (j+1) + " " + arrDocs[j]);
+                String[] oneDoc = arrDocs[j].split(" ");
+                for (int k = 0; k < oneDoc.length; k++) {
+                    System.out.println("В документе " + (j+1) + " первая ячейка " + oneDoc[k]);
+//TODO: Сделать двумерным
+                }
+            }
 
         } catch (FileNotFoundException e) {
-            System.out.println("Файл не найден" + e);;
+            System.out.println("Файл не найден");;
         } catch (IOException e) {
             System.out.println(e);;
         }
