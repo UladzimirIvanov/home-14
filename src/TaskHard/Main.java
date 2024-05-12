81ad6cacb452f20f89d5bc76715ca270e604a5e5
UladzimirIvanov <ivanouum@gmail.com>
2024-05-12 12:15:51 +0300

    TaskHard Result. Koe-kak rabotaet

--- src/TaskHard/Main.java
+++ src/TaskHard/Main.java
@@ -2,6 +2,7 @@ package TaskHard;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -10,28 +11,45 @@ public class Main {
         System.out.println("Укажите путь к файлу. Верный путь: src/TaskHard/docs.txt");
         Scanner scanner = new Scanner(System.in);
         String path = scanner.nextLine();
-        try (FileReader stream = new FileReader(path)){
+        try (FileReader stream = new FileReader(path)) {
             int i;
             StringBuilder txtDocs = new StringBuilder();
-            while ((i = stream.read()) != -1){
+            while ((i = stream.read()) != -1) {
                 txtDocs.append((char) i);
             }
-            //System.out.println(txtDocs);
-            //System.out.println();
             String[] arrDocs = txtDocs.toString().split("\n");
             for (int j = 0; j < arrDocs.length; j++) {
-                //System.out.println("Doc number " + (j+1) + " " + arrDocs[j]);
-                String[] oneDoc = arrDocs[j].split(" ");
-                for (int k = 0; k < oneDoc.length; k++) {
-                    System.out.println("В документе " + (j+1) + " первая ячейка " + oneDoc[k]);
-//TODO: Сделать двумерным
+                String[] oneDoc = arrDocs[j].trim().split(" ");
+                if ((oneDoc[0].equals("docnum") || oneDoc[0].equals("contract")) && oneDoc[1].length() == 15){
+                    //System.out.println(oneDoc[0] + " " + oneDoc[1]);
+                    try (FileWriter validfile = new FileWriter("src/TaskHard/validDocs.txt", true)){
+                        validfile.write(oneDoc[0] + " " + oneDoc[1] + "\n");
+                        validfile.flush();
+                    } catch (IOException e) {
+                        System.out.println(e);
+                    }
                 }
+                else {
+                    //System.out.println("Doc not val " + oneDoc[0] + " " + oneDoc[1]);
+                    try (FileWriter novalidfile = new FileWriter("src/TaskHard/noValidDocs.txt", true)){
+                        novalidfile.write(oneDoc[0] + " " + oneDoc[1] + "\n");
+                        novalidfile.flush();
+                    } catch (IOException e) {
+                        System.out.println(e);
+                    }
+                }
+                /*for (int k = 0; k < oneDoc.length; k++) {
+                    System.out.println(oneDoc[k]);
+                }*/
             }
 
+
         } catch (FileNotFoundException e) {
-            System.out.println("Файл не найден");;
+            System.out.println("Файл не найден");
+            ;
         } catch (IOException e) {
-            System.out.println(e);;
+            System.out.println(e);
+            ;
         }
     }
 }
